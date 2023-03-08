cfe55fb8979a2323a520eea4603e92bf0389af08
AlfredoKCL <alfredo.iacoangeli@gmail.com>
2023-03-08 22:20:24 +0000

    tested: my-script.sh directories created

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -3,6 +3,6 @@
 mkdir -p analysis docs data
 # add a README.md to each directory
 # the scripts directory already exists
-for my_directory in scripts analysis docs data;do
+for my_directory in scripts analysis docs data ; do
 touch ${my_directory}/README.md
-echo "# ${my_directory}" >> ${my_directory}/README.md done
+echo "# ${my_directory}" >> ${my_directory}/README.md ;  done
